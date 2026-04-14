5d221f1daac0ab97273f8959c1dc8b7db2035c05
NivMizzer <68729153+NivMizzer@users.noreply.github.com>
2026-04-14 17:09:03 +1000

    fixed oob texture access

--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -65,12 +65,12 @@ public class Simulation : MonoBehaviour
     }
     void RenderParticles()
     {
-        compute.Dispatch(2, Mathf.Min(Mathf.CeilToInt(particlesList.Count/1024f), 65535), 1, 1); //Render to Screen
+        compute.Dispatch(2, Mathf.Min(Mathf.CeilToInt(particlesList.Count/64f), 65535), 1, 1); //Render to Screen
     }
     
     void StepSimulation()
     {
-        compute.Dispatch(0, Mathf.Min(Mathf.CeilToInt(particlesList.Count/1024f), 65535), 1, 1); //Simulation Step
+        compute.Dispatch(0, Mathf.Min(Mathf.CeilToInt(particlesList.Count/64f), 65535), 1, 1); //Simulation Step
     }
     
     void SendPlanets()
